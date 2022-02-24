fdb68236da083a0fee899b9a140f925a206eca46
許庭愷 <91170807+kai1230@users.noreply.github.com>
2022-02-24 17:25:57 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    ///int a;
+    ///printf("使用者輸入一個數值:");
+    ///scanf("%d",&a);
+   ///printf("你讀到了 %d\n", a);
+
+    char c;
+    printf("請輸入一堆字: ");
+    scanf("%c",&c);
+    printf("你讀到了 %c\n",c);
+    return 0;
+}
